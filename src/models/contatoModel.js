963f8d56378f37ad432ce115b5e72c33d680eedf
Diogo Fernando Dotto <diogodotto@hotmail.com>
2024-07-06 20:13:29 -0300

    delete

--- src/models/contatoModel.js
+++ src/models/contatoModel.js
@@ -18,12 +18,6 @@ function Contato(body) {
     this.contatos = null;
 }
 
-Contato.buscaPorId = async function (id) {
-    if (typeof id !== 'string') return
-    const user = await ContatoModel.findById(id)
-    return user;
-}
-
 Contato.prototype.register = async function () {
     this.valida();
 
@@ -65,5 +59,25 @@ Contato.prototype.edit = async function (id) {
     if (this.errors.length > 0) return;
     this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
 }
+
+// Métodos estáticos
+Contato.buscaPorId = async function (id) {
+    if (typeof id !== 'string') return
+    const contato = await ContatoModel.findById(id)
+    return contato;
+}
+
+Contato.buscaContatos = async function () {
+    const contatos = await ContatoModel.find()
+        .sort( { criadoEm: -1 } )
+    return contatos;
+}
+
+Contato.delete = async function (id) {
+    if(typeof id !== 'string') return;
+    const contato = await ContatoModel.findByIdAndDelete(id);
+    return contato;
+}
+
 
 module.exports = Contato;
